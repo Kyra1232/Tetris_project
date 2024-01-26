62d489c0530332710a74f5149b1e997103ecddd7
Kyra1232 <151645502+Kyra1232@users.noreply.github.com>
2024-01-26 17:48:05 +0400

    Update main.py
    Счёт и основное меню

--- main.py
+++ main.py
@@ -100,6 +100,58 @@ T = [['.....',
       '..0..',
       '.....']]
 
+def draw_window(surface, grid, score=0, last_score=0):
+    surface.fill((0, 0, 0))
+
+    pygame.font.init()
+    font = pygame.font.Font(fontpath_mario, 65)
+    label = font.render('TETRIS', 1, (255, 255, 255))
+
+    surface.blit(label, (
+        (top_left_x + play_width / 2) - (label.get_width() / 2), 30))
+
+    font = pygame.font.Font(fontpath, 30)
+    label = font.render('SCORE   ' + str(score), 1, (255, 255, 255))
+
+    start_x = top_left_x + play_width + 50
+    start_y = top_left_y + (play_height / 2 - 100)
+
+    surface.blit(label, (start_x, start_y + 200))
+
+    label_hi = font.render('HIGHSCORE   ' + str(last_score), 1, (255, 255, 255))
+
+    start_x_hi = top_left_x - 240
+    start_y_hi = top_left_y + 200
+
+    surface.blit(label_hi, (start_x_hi + 20, start_y_hi + 200))
+
+    for i in range(row):
+        for j in range(col):
+            pygame.draw.rect(surface, grid[i][j],
+                             (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size), 0)
+
+    draw_grid(surface)
+    border_color = (255, 255, 255)
+    pygame.draw.rect(surface, border_color, (top_left_x, top_left_y, play_width, play_height), 4)
+
+
+def update_score(new_score):
+    score = get_max_score()
+
+    with open(filepath, 'w') as file:
+        if new_score > score:
+            file.write(str(new_score))
+        else:
+            file.write(str(score))
+
+
+def get_max_score():
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+        score = int(lines[0].strip())
+    return score
+
+
 def main_menu(window):
     run = True
     while run:
